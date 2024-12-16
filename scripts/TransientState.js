b6413c5b3aba185345dd02e2393abef93e7c31f2
Nate Z <ntzeitlin@gmail.com>
2024-12-16 10:31:14 -0600

    updated transientstate to reset after order submission

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -1,28 +1,40 @@
 // Set up Initial State 
-const transientState = {
-    "id": 0,
-    "metalId": 0,
-    "sizeId": 0,
-    "styleId": 0,
-    "orderTime": 0
+// const transientState = {
+//     "id": 0,
+//     "metalId": 0,
+//     "sizeId": 0,
+//     "styleId": 0,
+//     "orderTime": 0
+// }
+
+const transientState = new Map()
+
+const resetTransientState = () => {
+    transientState.set("id", 0)
+    transientState.set("metalId", 0)
+    transientState.set("sizeId", 0)
+    transientState.set("styleId", 0)
+    transientState.set("orderTime", 0)
 }
+
+resetTransientState()
 
 // Set up Setter Functions
 export const setMetalType = (chosenMetal) => {
-    transientState.metalId = chosenMetal
+    transientState.set("metalId", chosenMetal)
     console.log(transientState)
 }
 export const setSize = (chosenSize) => {
-    transientState.sizeId = chosenSize
+    transientState.set("sizeId", chosenSize)
     console.log(transientState)
 }
 export const setStyle = (chosenStyle) => {
-    transientState.styleId = chosenStyle
+    transientState.set("styleId", chosenStyle)
     console.log(transientState)
 }
 
 export const setOrderTime = (ordertime) => {
-    transientState.orderTime = ordertime
+    transientState.set("orderTime", ordertime)
     console.log(transientState)
 }
 
@@ -40,12 +52,12 @@ export const placeOrder = async () => {
         headers: {
             "Content-type": "application/json"
         },
-        body: JSON.stringify(transientState)
+        body: JSON.stringify(Object.fromEntries(transientState))
     }
 
     // Send the transient state to your API
     const response = await fetch("http://localhost:8088/orders", postOptions)
-
+    resetTransientState()
     const customEvent = new CustomEvent("newOrderPlaced")
     document.dispatchEvent(customEvent)
 
